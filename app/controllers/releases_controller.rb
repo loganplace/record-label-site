5a6d490c726f7dba057bd3cb3c7b358be5c83a70
Logan Place <place1l@cmich.edu>
2023-07-14 01:28:09 -0400

    changes routing for releases, adds gitignore

---
+++ app/controllers/releases_controller.rb
@@ -0,0 +1,12 @@
+class ReleasesController < ApplicationController
+
+    def index
+        @releases = Release.all
+      end
+    
+      def show
+        @release = Release.find(params[:id])
+      end
+    
+
+end
